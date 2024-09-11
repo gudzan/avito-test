b12833edb48ee23b12f0c18345f8d2d6c4a357a7
gudzan <andreeva.anva@gmail.com>
2024-09-11 11:11:48 +0300

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AdvertisementsList from "./pages/advertisments/advertisementsList";
 import AdvertisementPage from "./pages/advertisments/advertisementPage";
 import Orders from "./pages/orders/ordersList";
 import NavBar from "./componets/navBar/navBar";
-import "./App.css";
 import Header from "./componets/header/header";
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
         <Header/>
             <NavBar />
             <Routes>
-                <Route path="/" element={<AdvertisementsList />} />
+                <Route path="/" element={<Navigate to="/advertisements" replace />}/>
                 <Route path="/advertisements/:id" element={<AdvertisementPage />} />
                 <Route path="/advertisements" element={<AdvertisementsList />} />
                 <Route path="/orders" element={<Orders />} />   
